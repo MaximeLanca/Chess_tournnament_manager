7229fe75b15e1a388c063830d0c4a13bcc68ff91
MaximeLanca <lanca.maxime.ber@gmail.com>
2024-03-20 20:55:28 +0100

    Udpate save system Rounds

--- Tournament_manager/models/match.py
+++ Tournament_manager/models/match.py
@@ -7,6 +7,7 @@ class Match:
         self.player_1 = player_1
         self.player_2 = player_2
         self.winner = None
+        self.registration_number_match = None
 
     def define_match_winner(self, winner):
         self.winner = winner
@@ -23,7 +24,7 @@ class Match:
 
     def save_match_db(self):
         db = TinyDB("../Tournament_manager/data/tournaments/matches.json")
-        db.insert(self.to_dict())
+        self.registration_number_match = db.insert(self.to_dict())
 
     def to_dict(self):
         return {"Chess_national_ID_Player_1": self.player_1.chess_national_id,
--- Tournament_manager/models/round.py
+++ Tournament_manager/models/round.py
@@ -1,5 +1,29 @@
+from tinydb import TinyDB
+
+
 class Round:
 
-    def __init__(self, round_number, matches):
+    def __init__(self, round_number):
         self.round_number = round_number
-        self.matches = matches
+        self.matches = None
+        self.matches_list = []
+        self.round_list = None
+
+    def save_round_db(self):
+        db = TinyDB("../Tournament_manager/data/tournaments/round.json")
+        registration_round = db.insert(self.to_dict())
+        self.round_list = [registration_round]
+
+    def to_dict(self):
+        return {"Round": self.round_number,
+                "Matches_list": self.matches_list
+                }
+
+    @classmethod
+    def from_dict(cls, data):
+        return cls(data["Matches_list"])
+
+    def save_matches_list(self):
+        db = TinyDB("../Tournament_manager/data/tournaments/matches.json")
+        matches = db.all()[-1]
+        matches_list = [matches.doc_id]

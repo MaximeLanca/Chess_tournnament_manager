a93fc24aebf35177cff3657a3b211757c0aa9663
MaximeLanca <lanca.maxime.ber@gmail.com>
2024-05-06 22:33:08 +0200

    Update report fonction

--- Tournament_manager/__main__.py
+++ Tournament_manager/__main__.py
@@ -6,35 +6,31 @@ from views.menu import Menu
 
 def main():
     while True:
-        try:
-            user_answer = Menu.menu()
-            if user_answer == 1:
-                start_tournament = TournamentController()
-                start_tournament.run()
+        user_answer = Menu.menu()
+        if user_answer == 1:
+            start_tournament = TournamentController()
+            start_tournament.run()
 
-            elif user_answer == 2:
-                loaded_tournament = TournamentController()
-                loaded_tournament.load_tournament()
+        elif user_answer == 2:
+            loaded_tournament = TournamentController()
+            loaded_tournament.load_tournament()
 
-            elif user_answer == 3:
-                answer = Interface.ask_report_type()
-                tournament = TournamentController()
-                tournament.handle_request_for_report(answer)
+        elif user_answer == 3:
+            answer = Interface.ask_report_type()
+            tournament = TournamentController()
+            tournament.handle_request_for_report(answer)
 
-            elif user_answer == 4:
-                print("You have stopped the tournament manager.\nGood bye")
-                exit()
+        elif user_answer == 4:
+            print("You have stopped the tournament manager.\nGood bye")
+            exit()
 
-            elif user_answer == 5:
-                Player.search_all_players_db()
-                tournament = TournamentController()
-                tournament.speed_run()
+        elif user_answer == 5:
+            Player.search_all_players_db()
+            tournament = TournamentController()
+            tournament.speed_run()
 
-            else:
-                print("We didn't detect any entry input.\n")
-
-        except ValueError:
-            print("Error input")
+        else:
+            print("We didn't detect any entry input.\n")
 
 
 if __name__ == "__main__":
